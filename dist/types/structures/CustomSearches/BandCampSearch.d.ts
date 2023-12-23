df18062a593dade11e8f2d893b8a2156c64d7b6e
jeltcpss <98567980+jeltcpss@users.noreply.github.com>
2023-12-23 05:50:36 -0300

    Delete dist directory

--- dist/types/structures/CustomSearches/BandCampSearch.d.ts
+++
@@ -1,3 +0,0 @@
-import { Player } from "../Player";
-import { UnresolvedSearchResult } from "../Utils";
-export declare const bandCampSearch: (player: Player, query: string, requestUser: unknown) => Promise<UnresolvedSearchResult>;
